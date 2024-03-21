8088553765cddb03714743aef2d96c9aeb9d8407
kamrancompany <132373098+kamrancompany@users.noreply.github.com>
2024-03-21 23:23:51 +0500

    update:src/store/todos.txt update code

--- todo_1st_typeScript+React/src/store/todos.tsx
+++ todo_1st_typeScript+React/src/store/todos.tsx
@@ -1,9 +1,50 @@
-import { createContext } from "react";
+import { ReactNode, createContext, useContext, useState } from "react";
 
-export const todosContext = createContext (null)
+export type TodosProviderProps = {
+    children : ReactNode
+}
 
-export const TodosProvider = ({children}) =>{
- return <todosContext.Provider>
+export type Todo = {
+    id : string;
+    task: string;
+    completed : boolean;
+    createdAt: Date;
+}
+
+export type TodosContext ={
+    todos : Todo[];
+    handleAddToDo:(task:string) => void;
+}
+
+export const todosContext = createContext<TodosContext | null>(null)
+
+export const TodosProvider = ({children}:TodosProviderProps) =>{
+ 
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const handleAddToDo =(task:string)=>{
+       setTodos((prev)=>{
+        const newTodos:Todo[] = [
+            {
+                id: Math.random().toString(),
+                task:task,
+                completed:false,
+                createdAt:new Date()
+            },
+            ...prev
+        ]
+        return newTodos
+       })
+    }
+ return <todosContext.Provider value={{todos, handleAddToDo}}>
 {children}
  </todosContext.Provider>
+}
+
+export const useTodos = ()=>{
+    const todosConsumer = useContext(todosContext);
+    if(!todosConsumer){
+        throw new Error ("useTodos used outside of Provider")
+    }
+    return todosConsumer
 }
